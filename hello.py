15a0875003b8e2e86422da4d3c5aeed60110b813
Jimmy <Jimmy@JimmydeiMac.local>
2018-05-19 22:33:49 +0800

    add dynamic content into route

--- hello.py
+++ hello.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 # pylint: disable=invalid-name
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
+
+@app.route('/test')
+def test():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is {}</p>'.format(user_agent)
