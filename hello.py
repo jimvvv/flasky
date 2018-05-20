e93d77896c21e144d2325761d6313d2d1b26ea97
Jimmy <Jimmy@JimmydeiMac.local>
2018-05-20 22:48:35 +0800

    add new templates and install flask-bootstrap

--- hello.py
+++ hello.py
@@ -1,16 +1,13 @@
-from flask import Flask, request
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 # pylint: disable=invalid-name
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
-
-@app.route('/test')
-def test():
-    user_agent = request.headers.get('User-Agent')
-    return '<p>Your browser is {}</p>'.format(user_agent)
+    return render_template('user.html', name = name)
